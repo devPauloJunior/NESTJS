bd82d697e1c8e5cf97a19f6010ce3bf6a097e040
Paulo Junior <npaulo.ce@gmail.com>
2024-03-18 14:07:30 -0300

    Aula NEST JS com INSOMNIA 14/03

---
+++ second-project/src/User/user.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+
+@Controller('users')
+export class UserController {
+
+    @Post()
+    async create(@Body() body) {
+        return {body}
+    }
+
+    @Get()
+    async read() {
+        return { users:[] }
+    }
+
+    @Get(':id')
+    async readOne(@Param() param) {
+        return { user:{}, param }
+    }
+
+    @Put(':id')
+    async update(@Body() body, @Param() param) {
+        return {
+            method: 'PUT',
+            body,
+            param
+        }
+    }
+    
+    @Patch(':id')
+    async updatePartial(@Body() body, @Param() param) {
+        return {
+            method: 'PATCH',
+            body,
+            param
+        }
+    }
+
+    @Delete(':id')
+    async  delete(@Param() param) {
+        return { param }
+    }
+
+
+}
